f09ef410b3e509d9dc7c32a7b8a2c8263133ecfb
Richard Torruellas <rtorruellas@gmail.com>
2015-03-08 11:36:06 -0700

    adding es6 support

--- app/templates/util/jest_preprocessor.js
+++ app/templates/util/jest_preprocessor.js
@@ -1,8 +1,12 @@
 // preprocessor.js
 var ReactTools = require('react-tools');
+var babel = require('babel-core');
 module.exports = {
-  process: function(src) {
+  process: function(src, filename) {
     'use strict';
-    return ReactTools.transform(src);
+    src = ReactTools.transform(src);
+    if (filename.indexOf('node_modules') === -1 && babel.canCompile(filename)) {
+      return babel.transform(src, { filename: filename }).code;
+    }
   }
 };
